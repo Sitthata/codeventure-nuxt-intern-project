83dcbe9302ec1e2e581fb68de9bfa1d1a772e76c
devmarvelous <developer@marvelous.studio>
2024-11-10 20:59:01 +0700

    feat: implement authentication middleware and navigation history tracking

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -68,12 +68,7 @@ definePageMeta({
         );
       }
     },
-    function (to, from) {
-      if (to.params.chapterSlug === "1-chapter-1") {
-        return;
-      }
-      return navigateTo("/login");
-    },
+    'auth'
   ],
 });
 
