930918ac560799f40fb3391b4a50337031a8619a
devmarvelous <developer@marvelous.studio>
2024-11-12 00:22:17 +0700

    feat: add Pinia module for state management in Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/supabase"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxtjs/supabase",
+    "@pinia/nuxt",
+  ],
   supabase: {
     redirectOptions: {
       login: "/login",
