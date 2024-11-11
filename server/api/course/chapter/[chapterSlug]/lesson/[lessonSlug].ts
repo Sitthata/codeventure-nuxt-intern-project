3268098929d6868a6f6ceb99e4b2398c5adb058a
devmarvelous <developer@marvelous.studio>
2024-11-11 14:20:19 +0700

    feat: enhance lesson retrieval with chapter validation and error handling

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].ts
@@ -1,8 +1,30 @@
+import course from "~/server/courseData";
+
 export default defineEventHandler((event) => {
   const params = event.context.params;
   if (!params) {
-    throw new Error('Params are undefined');
+    throw new Error("Params are undefined");
   }
   const { chapterSlug, lessonSlug } = params;
-  return `Lesson "${lessonSlug}", "${chapterSlug}"`;
+
+  const chapter = course.chapters.find(
+    (chapter) => chapter.slug === chapterSlug
+  );
+
+  if (!chapter) {
+    throw createError({
+      statusCode: 404,
+      message: "Chapter not found",
+    });
+  }
+
+  const lesson = chapter.lessons.find((lesson) => lesson.slug === lessonSlug);
+
+  if (!lesson) {
+    throw createError({
+      statusCode: 404,
+      message: "Lesson not found",
+    });
+  }
+  return lesson
 });
