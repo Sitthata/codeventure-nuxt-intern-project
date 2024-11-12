8814408f42ab0e2dd9a6acb2b576609b34e5ef0e
devmarvelous <developer@marvelous.studio>
2024-11-12 19:43:02 +0700

    feat: replace Payment component with LazyPayment in landing page

--- pages/landing.vue
+++ pages/landing.vue
@@ -63,7 +63,7 @@
       </li>
     </ul>
   </Section>
-  <Payment v-if="showPayment" @close="showPayment = false" />
+  <LazyPayment v-if="showPayment" @close="showPayment = false" />
 </template>
 <script setup lang="ts">
 const course = await useCourse();
